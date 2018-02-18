3d6c96a40f9a8050f3593a5b7732f28587079d5d
Ron Li <runlin.li@gmail.com>
2018-02-17 23:06:41 -0500

    Resolved error of function not found due to using react.render rather than reactdom.render

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
 
+const Init = () => (
+  <h1>Hello World</h1>
+)
+
+
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(Counter),
+    React.createElement(Init),
     document.getElementById('mount')
   );
 });
