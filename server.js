f65796a8a9004c8260cbce6c98ad92103ca8a93b
Ron Li <runlin.li@gmail.com>
2018-02-21 22:50:47 -0500

    Changed server.js to route port correctly

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const compiler = webpack(webpackConfig);
 
 app.use(express.static(__dirname + '/www'));
+app.set('port', process.env.PORT || 8080);
 
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
@@ -18,7 +19,7 @@ app.use(webpackDevMiddleware(compiler, {
   historyApiFallback: true,
 }));
 
-const server = app.listen(3000, function() {
+const server = app.listen(app.get('port'), function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
