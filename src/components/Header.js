399a7e39650d393304b10a0572f24d5cad775433
Ron Li <runlin.li@gmail.com>
2019-01-31 21:14:01 -0500

    Add button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, } from '@material-ui/core';
+import { AppBar, Toolbar, ButtonBase, Typography, Fab } from '@material-ui/core';
+import logo from './icons/about.svg';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-const AppHeader = () => (
-  <AppBar position="static">
-    <Toolbar>
-      <Typography variant="title" color="inherit">
-        My React App Test
-      </Typography>
-      <div>
-        <Link to="/about/">About Me</Link>
-        <Link to="/home/">Home</Link>
-      </div>
-    </Toolbar>
-  </AppBar>
-);
+function AppHeader(props) {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <div>
+          <Link to="/about/">
+            <Fab color="primary" aria-label="About Me">
+              <img src={logo} alt="Logo"/>
+              About Me
+            </Fab>
+          </Link>
+        </div>
+        <div>
+          <Link to="/home/">Home</Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+} 
 
 export default AppHeader;
