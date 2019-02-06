e2c056e4d68d81fb7c0950df1cd3d9c54ee36fcc
ron.li <ron.li@jsitelecom.com>
2019-02-06 13:37:15 -0500

    Contact page complete

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,6 +9,10 @@ const styles = theme => ({
   media: {
     objectFit: 'cover',
     height: '218px'
+  },
+  button: {
+    backgroundColor: '#DFDFDF',
+    color: '#636363'
   }
 })
 
@@ -30,7 +34,7 @@ function ProjectCard(props) {
                 <Typography variant="h6">Project Title</Typography>
                 <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
                 <CardActions>
-                    <Button size="small" color="primary">
+                    <Button variant="contained" size="small" className={classes.button}>
                         Demo
                     </Button>
                 </CardActions>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Typography, withStyles, Toolbar, Avatar, Fab, Tooltip } from '@material-ui/core';
+import { Grid, Typography, withStyles, Toolbar, Avatar, Fab, Tooltip, TextField, Button } from '@material-ui/core';
 import MailIcon from '@material-ui/icons/Mail';
 import PhoneIcon from '@material-ui/icons/Phone';
-
-import icon from '../components/icons/about.svg'
+import SendIcon from '@material-ui/icons/Send';
 
 const styles = theme => ({
   header: {
@@ -21,68 +20,139 @@ const styles = theme => ({
     height: '100%',
     width: '100%'
   },
+  emailField: {
+    float: 'right'
+  },
   networkingDivider: {
     width: '80%',
     borderTop: '1px solid #636363',
     opacity: '0.3',
     marginBottom: '25px'
+  },
+  directDivider: {
+    marginTop: '25px',
+    height: '80%',
+    borderLeft: '1px solid #636363',
+    opacity: '0.3',
+  },
+  sendButton: {
+    marginTop: '5px',
+    // float: 'right'
+  },
+  send: {
+    height: '20px',
+    width: '20px',
+    margin: '0 0 0 10px'
   }
 })
 
-function Contact(props) {
-  const { classes } = props;
+class Contact extends React.Component {
+  state = {
+  }
+  
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
 
-  return (
-    <div>
-      <Typography variant="h4" gutterBottom className={classes.header}>Contact Me</Typography>
-      <Toolbar>
-        <Grid container spacing={24} direction="row">
+  render() {
+    const { classes } = this.props;
 
-          <Grid item xs className={classes.gridItem}>
-            <Grid container spacing={24} direction="column">
+    return (
+      <div>
+        <Typography variant="h4" gutterBottom className={classes.header}>Contact Me</Typography>
+        <Toolbar>
+          <Grid container spacing={24} direction="row">
 
-              <Grid item xs className={classes.gridItem}>
-                <Grid container alignItems="center">
-                  <Avatar className={classes.avatar}><MailIcon/></Avatar>
-                  <Typography variant="subtitle1">ronli@comli.com</Typography>
+            <Grid item xs={5} className={classes.gridItem}>
+              <Grid container spacing={24} direction="column">
+
+                <Grid item xs className={classes.gridItem}>
+                  <Typography variant="h6" gutterBottom className={classes.header}>Contact Info</Typography>
+                  <Grid container alignItems="center">
+                    <Avatar className={classes.avatar}><MailIcon/></Avatar>
+                    <Typography variant="subtitle1">ronli@comli.com</Typography>
+                  </Grid>
                 </Grid>
-              </Grid>
-              
-              <Grid item xs className={classes.gridItem}>
-                <Grid container alignItems="center">
-                  <Avatar className={classes.avatar}><PhoneIcon/></Avatar>
-                  <Typography variant="subtitle1">(123)-456-7890</Typography>
+                
+                <Grid item xs className={classes.gridItem}>
+                  <Grid container alignItems="center">
+                    <Avatar className={classes.avatar}><PhoneIcon/></Avatar>
+                    <Typography variant="subtitle1">(123)-456-7890</Typography>
+                  </Grid>
                 </Grid>
-              </Grid>
 
-              <Grid item xs>
-                <div className={classes.networkingDivider}></div>
-                <Typography variant="h6">Networking Platforms</Typography>
-                <Grid container alignItems="center">
-                  <Tooltip title="Github" aria-label="Github">
-                    <Fab className={classes.fab}>
-                      <img className={classes.avatarLinks} alt="Github" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
-                    </Fab>
-                  </Tooltip>
-                  <Tooltip title="LinkedIn" aria-label="LinkedIn">
-                    <Fab className={classes.fab}>
-                      <img className={classes.avatarLinks} alt="LinkedIn" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
-                    </Fab>
-                  </Tooltip>
-                  <Tooltip title="Devpost" aria-label="Devpost">
-                    <Fab className={classes.fab}>
-                      <img className={classes.avatarLinks} alt="Devpost" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
-                    </Fab>
-                  </Tooltip>
+                <Grid item xs>
+                  <div className={classes.networkingDivider}></div>
+                  <Typography variant="h6">Networking Platforms</Typography>
+                  <Grid container alignItems="center">
+                    <Tooltip title="Github" aria-label="Github">
+                      <Fab className={classes.fab}>
+                        <img className={classes.avatarLinks} alt="Github" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
+                      </Fab>
+                    </Tooltip>
+                    <Tooltip title="LinkedIn" aria-label="LinkedIn">
+                      <Fab className={classes.fab}>
+                        <img className={classes.avatarLinks} alt="LinkedIn" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
+                      </Fab>
+                    </Tooltip>
+                    <Tooltip title="Devpost" aria-label="Devpost">
+                      <Fab className={classes.fab}>
+                        <img className={classes.avatarLinks} alt="Devpost" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
+                      </Fab>
+                    </Tooltip>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
 
-        </Grid>
-      </Toolbar>
-    </div>
-  );
+            <Grid item xs={1}>
+              <div className={classes.directDivider}></div>
+            </Grid>
+
+            <Grid item xs className={classes.gridItem}>
+              <form>
+                <Typography variant="h6" gutterBottom className={classes.header}>Message Me Directly</Typography>
+                <TextField
+                  id="Name"
+                  label="Enter Your Name"
+                  placeholder="John Doe"
+                  margin="normal"
+                  variant="outlined"
+                />
+                <TextField
+                  id="Name"
+                  label="Enter Your Email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="JohnDoe@email.com"
+                  margin="normal"
+                  variant="outlined"
+                  className={classes.emailField}
+                />
+                <TextField
+                  id="Name"
+                  label="Enter Your Message"
+                  multiline
+                  rows="8"
+                  placeholder=""
+                  onChange={this.handleChange('multiline')}
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                />
+                <Button variant="contained" className={classes.sendButton}>
+                  Send
+                  <SendIcon className={classes.send}></SendIcon>
+                </Button>
+              </form>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </div>
+    );
+  }
 } 
 
 Contact.propTypes = {
