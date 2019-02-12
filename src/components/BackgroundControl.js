9c8d2baaef59c4ed0d493b91c0eed180599c73ab
ron.li <ron.li@jsitelecom.com>
2019-02-12 17:17:08 -0500

    Add looping cloud

--- src/components/BackgroundControl.js
+++ src/components/BackgroundControl.js
@@ -20,12 +20,13 @@ class BackgroundControl extends React.Component {
       cloud1Pos: -15,
       clouds: [],
       availableIds: [],
-      maxID: 0
+      maxID: 0,
+      top: '20%'
     }
   }
 
   componentDidMount() {
-    this.updatePositions(10, 10);
+    this.updatePositions(20, 20000);
     this.updateWindowDimensions();
     window.addEventListener('resize', this.updateWindowDimensions);
   }
@@ -35,27 +36,33 @@ class BackgroundControl extends React.Component {
   }
 
   updatePositions(pos, time) {
-    // setInterval(
-    //   () => {
-        // Grab a unique id, also dont forget to put it back after use!
-        var id;
-        if (this.state.availableIds.length <= 0) {
-          id = this.state.maxID;
-          this.setState({maxID: id + 1});
-        } else {
-          id = this.state.availableIds[0];
-          this.state.availableIds.shift();  // Might not work and require setstate instead
-        }
-        // Add new cloud
-        this.setState(state => ({
-          clouds: [
-            ...state.clouds,
-            { id: id, top: pos },
-          ],
-        }));
-    //   },
-    //   time * 1000
-    // )
+    // Grab a unique id, also dont forget to put it back after use!
+    var id;
+    if (this.state.availableIds.length <= 0) {
+      id = this.state.maxID;
+      this.setState({maxID: id + 1});
+    } else {
+      id = this.state.availableIds[0];
+      this.state.availableIds.shift();  // Might not work and require setstate instead
+    }
+
+    // Update the new cloud's position
+    this.setState({top: pos+'%'});
+
+    // Add new cloud
+    this.setState(state => ({
+      clouds: [
+        ...state.clouds,
+        { id: id, top: pos, time: time },
+      ],
+    }));
+
+    // Wait until animation finishes
+    setTimeout(
+      () => {
+        this.updatePositions(pos, time);
+      }, time + 1500
+    )
   }
 
   updateWindowDimensions() {
@@ -67,20 +74,22 @@ class BackgroundControl extends React.Component {
   render() {
     const { classes } = this.props;
     const { clouds } = this.state;
+    var { top } = this.state;
 
     return (
       <div>
         {/* <img style={{left: this.state.cloud1Pos+'%'}} className={classes.clouds} src="http://ronli.comli.com/Icons/cloud.png"/> */}
         <TransitionGroup>
-          {clouds.map(({ id, top }) => (
+          {clouds.map(({ id, pos, time }) => {
+            return (
             <CSSTransition
               key={id}
-              timeout={10000}
+              timeout={time}
               classNames="clouds"
             >
-              <img className="clouds" style={{top: {top}+'%'}} src="http://ronli.comli.com/Icons/cloud.png"/>
+              <img className="clouds" style={{top: top}} src="http://ronli.comli.com/Icons/cloud.png"/>
             </CSSTransition>
-          ))}
+          )})}
         </TransitionGroup>
       </div>
     );
