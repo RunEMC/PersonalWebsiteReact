7f28ab95844e6b6a7a35f257911676c54906d9d5
ron.li <ron.li@jsitelecom.com>
2019-02-13 15:12:06 -0500

    Finish cloud animations

--- src/components/BackgroundControl.js
+++ src/components/BackgroundControl.js
@@ -2,8 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Typography, withStyles, CardMedia, CardContent, CardActions, Button, CardActionArea } from '@material-ui/core';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
 
 import './clouds.css';
+
+// Move this to separate file
+const configs = {
+  cloudVerticalVariability: 5,
+  cloudSizeVariability: 5,
+  cloudSpeedVariability: 10000,
+  cloudVerticalStart: 20,
+  cloudAvgSize: 10,
+  cloudAvgSpeed: 40000,   // As miliseconds in time taken to complete animation
+  cloudStartDelayMax: 10000,
+  cloudStartDelayMin: 1000,
+}
 
 const styles = theme => ({
   container: {
@@ -17,17 +30,16 @@ class BackgroundControl extends React.Component {
     super(props);
     this.state = {
       windowHeight: 0,
-      cloud1Pos: -15,
       clouds: [],
       availableIds: [],
-      maxID: 0,
-      top: '20%'
+      maxID: 0
     }
   }
 
   componentDidMount() {
-    this.updatePositions(20, 20000);
-    this.updateWindowDimensions();
+    // this.updateWindowDimensions();
+    this.createClouds();
+    // this.createNewCloudLoop(20, 10, 20000);
     window.addEventListener('resize', this.updateWindowDimensions);
   }
 
@@ -35,61 +47,79 @@ class BackgroundControl extends React.Component {
     window.removeEventListener('resize', this.updateWindowDimensions);
   }
 
-  updatePositions(pos, time) {
-    // Grab a unique id, also dont forget to put it back after use!
-    var id;
-    if (this.state.availableIds.length <= 0) {
-      id = this.state.maxID;
-      this.setState({maxID: id + 1});
-    } else {
-      id = this.state.availableIds[0];
-      this.state.availableIds.shift();  // Might not work and require setstate instead
-    }
+  async createClouds() {
+    var pageSizeAvailable = 100 - configs.cloudVerticalStart - configs.cloudVerticalVariability;
+    var top = this.getRandBetween(configs.cloudVerticalStart, pageSizeAvailable);
+    var size = configs.cloudAvgSize + this.getRandBetween(0, configs.cloudSizeVariability * 2) - configs.cloudSizeVariability;
+    var speed = configs.cloudAvgSpeed + this.getRandBetween(0, configs.cloudSpeedVariability * 2) - configs.cloudSpeedVariability;
+    var delay = this.getRandBetween(configs.cloudStartDelayMin, configs.cloudStartDelayMax);
+    // Create cloud
+    this.createNewCloudLoop(top, size, speed, delay);
+    console.log("Top: " + top + " Size: " + size + " Speed: " + speed + " Delay: " + delay);
 
-    // Update the new cloud's position
-    this.setState({top: pos+'%'});
+    // Wait for a bit before sending the next one
+    setTimeout(
+      () => {
+        this.createClouds();
+      },
+      delay
+    )
+  }
+
+  async createNewCloudLoop(pos, size, time, delay) {
+    var id = uuid();
 
     // Add new cloud
     this.setState(state => ({
       clouds: [
         ...state.clouds,
-        { id: id, top: pos, time: time },
+        { id: id, top: pos+'%', size: size+'%', time: (time/1000)+'s' },
       ],
     }));
 
-    // Wait until animation finishes
+    // Wait until animation finishes before deleting
     setTimeout(
       () => {
-        this.updatePositions(pos, time);
-      }, time + 1500
+        this.setState(state => ({
+          clouds: state.clouds.filter(
+            cloud => cloud.id !== id
+          ),
+        }));
+      }, time + 2500
     )
   }
 
+  // Returns a random integer between min and max inclusive
+  getRandBetween(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    var ret = Math.floor((Math.random() * (max - min + 1)) + min);
+    // console.log("Min: " + min + " Max: " + max + " Ret: " + ret);
+    return ret;
+  }
+
   updateWindowDimensions() {
-    this.setState({
-      windowHeight: window.innerHeight
-    })
+    // this.setState({
+    //   windowHeight: window.innerHeight
+    // })
   }
   
   render() {
-    const { classes } = this.props;
     const { clouds } = this.state;
-    var { top } = this.state;
 
     return (
       <div>
-        {/* <img style={{left: this.state.cloud1Pos+'%'}} className={classes.clouds} src="http://ronli.comli.com/Icons/cloud.png"/> */}
         <TransitionGroup>
-          {clouds.map(({ id, pos, time }) => {
-            return (
+          {clouds.map(({ id, top, size, time }) => (
             <CSSTransition
               key={id}
               timeout={time}
               classNames="clouds"
+              exit={false}
             >
-              <img className="clouds" style={{top: top}} src="http://ronli.comli.com/Icons/cloud.png"/>
+              <img style={{top: top, height: size, transitionDuration: time}} src="http://ronli.comli.com/Icons/cloud.png"/>
             </CSSTransition>
-          )})}
+          ))}
         </TransitionGroup>
       </div>
     );
