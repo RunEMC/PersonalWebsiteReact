ae465a61ef5e822f6e28d83c9185ecc553c6bfe6
Ron Li <ron.li@jsitelecom.com>
2019-02-22 16:30:10 -0500

    Update about content

--- src/pages/About.js
+++ src/pages/About.js
@@ -40,7 +40,9 @@ function About(props) {
               </Grid>
               <Grid item xs>
                 <Typography variant="body1" className={classes.text}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  My name is Ron Li, I am a fourth year Double Degree student studying Computer Science at the University of Waterloo and Business Administration at Wilfrid Laurier University. I am currently on a co-op work term as a software developer at JSI Telecom while in the past I've worked at Manulife and Polaris Transportation Group. As a software developer, I am experienced in the following: 
+                  {/* Format this into a table with Language/Framework/Tools at the top and familiarity as the row headers */}
+                  C, C++, C#, Python, React, Angular, Node JS, Git, Bash, VIM, Visual Studio, Latex...
                 </Typography>
               </Grid>
             </Grid>
@@ -48,7 +50,7 @@ function About(props) {
 
           <Grid item xs>
             <Typography className={classes.text}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              On my free time I enjoy watching movies/TV shows, playing video games and my guitar. I also own a sizable collection of tabletop games that is ever expanding including classics such as Pandemic, Catan and Avalon. Occasionally, I would attend hackations if possible in order to keep my development skills up to date and to try and complete a project that is on my backlog of ideas.
             </Typography>
           </Grid>
 
